9d77e0b855d2d873f3f4fda1260e3cdba6d43eb4
luzvelam <166964610+luzvelam@users.noreply.github.com>
2024-11-07 15:08:53 -0800

    Update videos.s

--- videos/videos.s
+++ videos/videos.s
@@ -1,6 +1,9 @@
 10- ejercicios 1 parte
+https://asciinema.org/a/Sc47XmGQH9WIxP9BtVVMwu8CQ
+
+10 -ejercicios 2 parte
+
+https://asciinema.org/a/U87bs0MxV6f3dA0MVN3dlofEw
 
 
-10 -ejercicios2 parte
-
-10 - ejercicios 
+10 - ejercicios 3 parte
