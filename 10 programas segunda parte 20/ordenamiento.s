5f9f6f49aa0ac4f3159bea017cc3a8856c511d11
luzvelam <166964610+luzvelam@users.noreply.github.com>
2024-11-07 16:16:08 -0800

    Update ordenamiento.s

--- 10 programas segunda parte 20/ordenamiento.s
+++ 10 programas segunda parte 20/ordenamiento.s
@@ -4,6 +4,48 @@
 // Programa en ARM64 Assembly para ordenar un array de enteros con el método de selección
 // Este programa imprimirá el array ordenado después de realizar el ordenamiento
 
+//c#
+/*
+#include <stdio.h>
+
+void selectionSort(int arr[], int n) {
+    int i, j, minIndex, temp;
+    for (i = 0; i < n-1; i++) {
+        minIndex = i;
+        for (j = i+1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        // Intercambia el elemento mínimo con el primer elemento
+        temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+void printArray(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printf("Array original: \n");
+    printArray(arr, n);
+
+    selectionSort(arr, n);
+
+    printf("Array ordenado: \n");
+    printArray(arr, n);
+    return 0;
+}
+*/
+
 .data
     prompt:       .asciz "Ordenando el array usando el método de selección...\n"
     sortedMsg:    .asciz "Array ordenado:\n"
