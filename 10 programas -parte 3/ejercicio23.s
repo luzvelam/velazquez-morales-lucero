6f34b52d7e7c76b5bb45a1bee5e2d39e3646980e
luzvelam <166964610+luzvelam@users.noreply.github.com>
2024-11-10 21:40:18 -0800

    Create ejercicio23.s

---
+++ 10 programas -parte 3/ejercicio23.s
@@ -0,0 +1,9 @@
+//lucero velazquez morales no.control 22210362
+//ejercicio 23- 	Conversión de entero a ASCII
+//fecha: 10-11-2024
+
+
+/*
+Este programa en C++ pide al usuario que ingrese su nombre, luego calcula la longitud de la cadena ingresada y muestra el resultado.
+Utiliza la biblioteca <string> para manejar la entrada del usuario y calcular la longitud de la cadena.
+*/
