32140ff94ff67bea209af2906cfd81c7853cc7a9
luzvelam <166964610+luzvelam@users.noreply.github.com>
2024-11-10 21:48:04 -0800

    Update ejercicio24.s

--- 10 programas -parte 3/ejercicio24.s
+++ 10 programas -parte 3/ejercicio24.s
@@ -1,2 +1,18 @@
 //lucero velazquez morales no.control 22210362
-//ejercicio 24
+//ejercicio 24 - Calcular la longitud de una cadena
+
+
+/*
+#include <stdio.h>
+#include <string.h>  // Necesario para usar strlen()
+
+int main() {
+    const char *str = "Hola, Mundo!";
+    size_t length = strlen(str);  // Calcular la longitud de la cadena
+
+    printf("La longitud de la cadena es: %zu\n", length);
+
+    return 0;
+}
+
+*/
