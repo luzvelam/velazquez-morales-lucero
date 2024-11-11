36fb16c0ee3b8c8bc0b63fe324ce6d78f412cef1
luzvelam <166964610+luzvelam@users.noreply.github.com>
2024-11-10 21:52:59 -0800

    Update ejercicio25.s

--- 10 programas -parte 3/ejercicio25.s
+++ 10 programas -parte 3/ejercicio25.s
@@ -1,3 +1,33 @@
 //lucero velazquez morales no.control 22210362
-//ejercicio 25 -
+//ejercicio 25 -Contar vocales y consonantes
+//fecha:10-11-2024
+//
+
+/*
+
+#include <stdio.h>
+
+int string_length(const char *str) {
+    int length = 0;
+
+    // Contar los caracteres hasta encontrar el carácter nulo '\0'
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    return length;
+}
+
+int main() {
+    const char *str = "Hola, Mundo!";
+    int length = string_length(str);  // Calcular la longitud manualmente
+
+    printf("La longitud de la cadena es: %d\n", length);
+
+    return 0;
+}
+
+*/
+
+
 
