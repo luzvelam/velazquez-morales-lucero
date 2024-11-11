efd916923631197e73359c2b9b645d873ae28f93
luzvelam <166964610+luzvelam@users.noreply.github.com>
2024-11-10 22:47:08 -0800

    Update videos.s

--- videos/videos.s
+++ videos/videos.s
@@ -6,5 +6,12 @@ https://asciinema.org/a/Sc47XmGQH9WIxP9BtVVMwu8CQ
 https://asciinema.org/a/U87bs0MxV6f3dA0MVN3dlofEw
 
 
-10 - ejercicios parte 3
-ejercicio 21 -https://asciinema.org/a/9ojLXbwpgtgVy74l5bL5RivZP
+10 - ejercicios parte 3 -//corrida del debugger GEF
+ejercicio 21 
+https://asciinema.org/a/9ojLXbwpgtgVy74l5bL5RivZP
+
+ejercicio 22
+https://asciinema.org/a/ppOJjF0Me7mOgYT7VVg60dIep
+
+ejercicio 23
+
