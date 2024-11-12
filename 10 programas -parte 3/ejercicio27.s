aae6d8da21814cd4b2f541648895529a1366dd48
luzvelam <166964610+luzvelam@users.noreply.github.com>
2024-11-11 17:04:51 -0800

    Update ejercicio27.s

--- 10 programas -parte 3/ejercicio27.s
+++ 10 programas -parte 3/ejercicio27.s
@@ -1,8 +1,8 @@
 //lucero velazquez morales no.control 22210362
 //Desplazamientos a la izquierda y derecha
-//Fecha: 10-11-2024
+//Fecha: 11-11-2024
 // Programa en ARM64 Assembly 
-
+//https://asciinema.org/a/JtcE5HFvQQd44QjRmtbUzEDiz
 
 /*
 #include <stdio.h>
